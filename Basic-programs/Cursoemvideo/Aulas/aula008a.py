151f8567abc66f36e3d03dc67ccc652cdab57fe3
Leandro Barbosa <leandroh_99@outlook.com>
2023-08-29 19:20:13 -0300

    aulas

---
+++ Basic-programs/Cursoemvideo/Aulas/aula008a.py
@@ -0,0 +1,5 @@
+# introducing the usage of modules
+from math import sqrt
+num = int(input('Digite um numero = '))
+raiz = sqrt(num)
+print(f'A raiz de {num} é igual a {raiz:.2f}')
---
+++ Basic-programs/Cursoemvideo/Aulas/aula008b.py
@@ -0,0 +1,6 @@
+import random
+num = random.random()
+print(num)
+
+num2 = random.randint(1, 100)
+print(num2)
